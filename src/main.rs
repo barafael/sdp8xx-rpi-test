186b61c607d8b4c0e39cc694e7f0769a7fe41bf1
Rafael Bachmann <mediumendian@gmail.com>
2021-01-10 00:09:28 +0100

    Add more linux tests

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,36 @@ fn main() {
 
     let product_id = sdp.read_product_id().unwrap();
     println!("{:?}", product_id);
+
+    println!("Taking 10 triggered samples");
+    for _ in 0..=10 {
+        if let Ok(m) = sdp.read_sample_triggered() {
+            println!("{:?}", m);
+        } else {
+            println!("Error!");
+        }
+    }
+
+    let mut sdp_sampling = match sdp.start_sampling_differential_pressure(true) {
+        Ok(s) => s,
+        Err(e) => {
+            println!("{:?}", e);
+            loop {}
+        }
+    };
+    for _ in 0..=50 {
+        let result = sdp_sampling.read_continuous_sample();
+        match result {
+            Ok(r) => println!("{:?}", r),
+            Err(e) => println!("Error while getting result: {:?}", e),
+        }
+    }
+    let mut idle_sensor = sdp_sampling.stop_sampling().unwrap();
+    loop {
+        if let Ok(m) = idle_sensor.read_sample_triggered() {
+            println!("{:?}", m);
+        } else {
+            println!("Error!");
+        }
+    }
 }
